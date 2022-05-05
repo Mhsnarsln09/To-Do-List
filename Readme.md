cd108110933d68c954c289e9cf3d7eae843f58b3
Muhsin ARSLAN <mhsnarsln09@gmail.com>
2022-05-05 03:30:24 +0300

    First

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Ödev-2 To DO List
+## Kodluyoruz.org Frontend Bootcamp Hazırlık Programı 6.Hafta Ödev 2 Yapılacaklar Listesi Projesi.
+![img](https://raw.githubusercontent.com/Kodluyoruz/taskforce/main/javascript/javascript-temel/odev2/figures/todolist.gif)
---
+++ js/js.js
@@ -0,0 +1,37 @@
+function newElement () {
+    const taskInput = document.getElementById("task");
+    let text = taskInput.value;
+    if (text==""){
+        $('#liveToastError').toast('show')
+        return;
+        // Boş değer girilirse Listeye boş ekleme yapamazsınız! yazısını bootstrap ile alıcaz
+    }
+    else{
+        let ul = document.getElementById("list");
+        ul.innerHTML += `<li class='checked::before' onClick=changeChecked()>
+         ${text}
+          <button class='btn btn-danger float-right' id='delete' onclick='removeItem()'>X</button>
+        </li>`;
+
+        $('#liveToast').toast('show')
+        // Değer ekleyeceğiz
+    }
+
+};
+
+function removeItem() {
+    event.currentTarget.parentElement.remove();
+    // Değeri kaldırma işlemi
+}
+
+function changeChecked() {
+    if (event.currentTarget.className ==='checked::before'){
+        event.currentTarget.className = 'checked';
+        
+    }
+    else{
+        event.currentTarget.className = 'checked::before';
+    }
+    
+    // Değeri işaretleme işlemi
+}
