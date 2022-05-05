25d9887e344853af1e0dc96b596b17ce59f527e8
Muhsin ARSLAN <mhsnarsln09@gmail.com>
2022-05-05 03:56:17 +0300

    js

--- js/js.js
+++ js/js.js
@@ -1,23 +1,23 @@
-function newElement () {
-    const taskInput = document.getElementById("task");
-    let text = taskInput.value;
-    if (text==""){
+function newElement() {
+    const taskInput = document.getElementById("task")
+    let text = taskInput.value
+    if (text == ""){
         $('#liveToastError').toast('show')
         return;
-        // Boş değer girilirse Listeye boş ekleme yapamazsınız! yazısını bootstrap ile alıcaz
+        // Boş değer girilmezse Listeye boş ekleme yapamazsınız! yazısını bootstrap ile alıcaz
     }
-    else{
-        let ul = document.getElementById("list");
-        ul.innerHTML += `<li class='checked::before' onClick=changeChecked()>
-         ${text}
-          <button class='btn btn-danger float-right' id='delete' onclick='removeItem()'>X</button>
-        </li>`;
-
-        $('#liveToast').toast('show')
-        // Değer ekleyeceğiz
-    }
-
-};
+    else {
+    let ul = document.getElementById("list")
+    ul.innerHTML += `<li class='checked::before' onClick=changeChecked()>
+    ${text}
+     <button class='btn btn-danger float-right' id='delete' onclick='removeItem()'>X</button>
+   </li>`
+   // Değer ekleme işlemi
+   $('#liveToast').toast('show')
+// Değer girilirse Listeye eklendi. yazısını bootsrap ile alıcaz
+}
+    
+}
 
 function removeItem() {
     event.currentTarget.parentElement.remove();
